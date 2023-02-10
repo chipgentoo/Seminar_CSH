c58f0bde3d89ca9ce85c942969f4e7e425b0a3ca
chipgentoo <chip.gentoo@gmail.com>
2023-02-10 23:23:28 +0300

    Home work. Exercise 2. MAX of two

---
+++ HomeWork_Exercise_2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.WriteLine("максимальное число: " + number1);
+}
+else
+{
+    Console.WriteLine("максимальное число: " + number2);
+}
