a90f661d87a9e877897ef008d0c242f9b31fb7c6
chipgentoo <chip.gentoo@gmail.com>
2023-02-11 00:04:19 +0300

    Home work. Exercise 6. Even number

---
+++ HomeWork_Exercise_6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("Да, число " + number + " является четным");
+}
+else
+{
+    Console.WriteLine("Нет, число " + number + " НЕ является четным");
+}
