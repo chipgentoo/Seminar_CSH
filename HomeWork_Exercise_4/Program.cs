5504d21520e90b2c86d6b695ef2e9777f94d6f20
chipgentoo <chip.gentoo@gmail.com>
2023-02-10 23:47:47 +0300

    Home work. Exercise 4. MAX of three

---
+++ HomeWork_Exercise_4/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int max = number1; // максимальное число - первое
+
+if (number2 > max) // второе число больше максимального (первого)
+{
+    max = number2;
+}
+
+if (number3 > max) // третье число больше максимального (первого и второго)
+{
+    max = number3;
+}
+
+Console.WriteLine("Максимальное число: " + max);
