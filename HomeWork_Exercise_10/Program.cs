3c63e2d70721b25eba9f5ec2861bdc96b4fafecd
chipgentoo <chip.gentoo@gmail.com>
2023-02-15 00:27:19 +0300

    Home work. Exercise 10. Second digit of a three-digit number

---
+++ HomeWork_Exercise_10/Program.cs
@@ -0,0 +1,27 @@
+﻿int checkNumber(string? strNumber)
+{
+    int number = 0;
+    try
+    {
+        return number = Convert.ToInt32(strNumber);
+    }
+    catch (System.Exception)
+    {
+        return number = (-1);
+    }
+}
+
+Console.WriteLine("Вывод 2й цифры 3х-значного числа!!!");
+Console.WriteLine("Ведите положительное целое число от 100 до 999");
+
+int value = checkNumber(Console.ReadLine());
+if (value >= 100 && value <= 999)
+{
+    int twoNumber = (value % 100) / 10;
+    Console.Write($"Вторая цифра числа {value}: {twoNumber}");
+}
+else
+{
+    Console.WriteLine("Введены не верные данные!!!");
+}
+Console.WriteLine();
