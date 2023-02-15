3bfefc82a9d2909ac27818ac76f83a8aa3e2bfa6
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 <chip.gentoo@gmail.com>
2023-02-15 16:07:10 +0300

    Working day or day off

---
+++ HomeWork_Exercise_15/Program.cs
@@ -0,0 +1,31 @@
+﻿int checkNumber(string? strNumber) // проверка на то, что это не строка
+{
+    int number = 0;
+    try
+    {
+        return number = Convert.ToInt32(strNumber);
+    }
+    catch (System.Exception)
+    {
+        return (-1);
+    }
+}
+
+Console.WriteLine("Введите номер дня недели (1-7)");
+
+int numDay = checkNumber(Console.ReadLine());
+if (numDay > 0 && numDay < 8)
+{
+    if (numDay == 6 || numDay == 7)
+    {
+        Console.WriteLine("Это выходной день");
+    }
+    else
+    {
+        Console.WriteLine("Это рабочий день");
+    }
+}
+else
+{
+    Console.WriteLine("Не корректный номер дня недели");
+}
