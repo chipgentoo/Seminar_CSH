7e41877f8c065855c939f4a65c220a8c10649d14
chipgentoo <chip.gentoo@gmail.com>
2023-02-17 22:59:10 +0300

    Polindrom 5 numerical

---
+++ HomeWork_Exercise_19/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите 5ти-значное число: ");
+string? strNumber = Console.ReadLine();
+int number;
+if (int.TryParse(strNumber, out number) && number > 9999 && number < 99999)
+{
+    //
+    if (number / 10000 == number % 10 && (number / 1000) % 10 == (number % 100) / 10)
+    {
+        Console.WriteLine("Это полиндром 5ти-значного числа!");
+    }
+    else
+    {
+        Console.WriteLine("Это НЕ полиндром 5ти-значного числа!");
+    }
+}
+else
+{
+    Console.WriteLine("Это НЕ 5ти-значное число!");
+}
