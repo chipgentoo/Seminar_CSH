52194a0269c92b19e8749184c3d9cb8112fb8e8b
chipgentoo <chip.gentoo@gmail.com>
2023-02-18 00:39:00 +0300

    Сube numbers table

---
+++ HomeWork_Exercise_23/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Таблица кубов числа");
+
+Console.Write("Введите число: ");
+string? strNumber = Console.ReadLine();
+int number;
+if (int.TryParse(strNumber, out number))
+{
+    for (int i = 1; i <= number; i++)
+    {
+        Console.Write($"Куб {i} = {Math.Pow(i,3)}\t\t");
+        if (i%5==0)
+        {
+            Console.WriteLine(); // вывод в 5 столбцов
+        }
+    }
+}
+else
+{
+    Console.WriteLine("Это числовое значение!");
+}
+Console.WriteLine();
