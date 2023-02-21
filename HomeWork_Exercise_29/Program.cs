5f10c5086de45702388d9eeee45037854c988024
chipgentoo <chip.gentoo@gmail.com>
2023-02-22 01:19:14 +0300

    Input and Random arrays

---
+++ HomeWork_Exercise_29/Program.cs
@@ -0,0 +1,31 @@
+﻿int GetNumberConsole(string message)
+{
+    int number = 0;
+    Console.WriteLine(message);
+    string? strValue = Console.ReadLine();
+    if (!int.TryParse(strValue, out number))
+    {
+        Console.WriteLine("Введите цифровое значение!");
+        Environment.Exit(-1);
+    }
+    return number;
+}
+
+// этот массив вводится руками
+int sizeArray = GetNumberConsole("Введите размер массива: ");
+int[] array = new int[sizeArray];
+for (int i = 0; i < sizeArray; i++)
+{
+    array[i] = GetNumberConsole($"Введите значение {i}-го элемента");
+}
+Console.WriteLine($"{string.Join("; ", array)}");
+
+//этот массив - 8 случайных чисел
+Console.WriteLine("\nМассив из 8 случайных чисел!");
+int[] array2 = new int[8];
+for (int i = 0; i < 8; i++)
+{
+    array2[i] = new Random().Next(10, 1000);
+}
+Console.WriteLine($"{string.Join("; ", array2)}");
+
