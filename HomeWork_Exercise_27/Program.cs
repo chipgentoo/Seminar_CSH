99e91a0799310e9bd3eaa206f37b69706c820d21
chipgentoo <chip.gentoo@gmail.com>
2023-02-22 00:50:53 +0300

    Sum all digit

---
+++ HomeWork_Exercise_27/Program.cs
@@ -0,0 +1,17 @@
+﻿int GetNumberConsole(string message)
+{
+    Console.WriteLine(message);
+    string? strValue = Console.ReadLine();
+    int number = Convert.ToInt32(strValue);
+    return number;
+}
+
+int value = GetNumberConsole("Введите число: ");
+if (value < 0) value *= (-1);
+int sum = 0;
+while (value > 0)
+{
+    sum += value % 10;
+    value /= 10;
+}
+Console.WriteLine($"Сумма цифр равна {sum}");
