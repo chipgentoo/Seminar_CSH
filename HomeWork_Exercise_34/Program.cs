01b86a34a26f66e240b6d9bb4b769e0a4f1a8c04
chipgentoo <chip.gentoo@gmail.com>
2023-02-24 23:14:03 +0300

    Even numbers in  array

---
+++ HomeWork_Exercise_34/Program.cs
@@ -0,0 +1,41 @@
+﻿int GetNumberConsole(string message)
+{
+    int number = 0;
+    Console.WriteLine(message);
+    string? strValue = Console.ReadLine();
+    if (!int.TryParse(strValue, out number))
+    {
+        Console.WriteLine("Введите цифровое значение!");
+        Environment.Exit(-1);
+    }
+    return number;
+}
+
+int[] fillArray(int[] array, int minValue, int maxValue)
+{
+    int size = array.Length;
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+int evenNumberArray(int[] array)
+{
+    int size = array.Length;
+    int count = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (array[i]%2==0) count++;
+    }
+    return count;
+}
+
+int size = GetNumberConsole("Введите размер массива: ");
+int[] array = new int[size];
+int minValue = 100;
+int maxValue = 999;
+array = fillArray(array, minValue, maxValue);
+
+Console.Write($"В массиве: [{string.Join("; ",array)}]\nкол-во четных чисел: {evenNumberArray(array)}\n");
