bb44bd9cd1b8526bc37ebaedc5f05bdf9741c574
chipgentoo <chip.gentoo@gmail.com>
2023-03-02 00:34:49 +0300

    Number to binary

---
+++ HomeWork_Exercise_42/Program.cs
@@ -0,0 +1,17 @@
+﻿int GetNumberConsole(string message)
+{
+    int number = 0;
+    Console.Write(message);
+    string? strValue = Console.ReadLine();
+    if (!int.TryParse(strValue, out number))
+    {
+        Console.WriteLine("Введите числовое значение!");
+        Environment.Exit(-1);
+    }
+    return number;
+}
+
+int intValue = GetNumberConsole("Введите десятичное числовое значение: ");
+
+string binLine = Convert.ToString(intValue, 2);
+Console.WriteLine($"Бинарный формат числа {intValue} = 0x{binLine}");
