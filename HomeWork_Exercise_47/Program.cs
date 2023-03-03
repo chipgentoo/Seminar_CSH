abc6c6fc3e07e201fe06559fb395d532ff43ec7b
chipgentoo <chip.gentoo@gmail.com>
2023-03-03 22:51:35 +0300

    2D Matrix random size from MIN to MAX

---
+++ HomeWork_Exercise_47/Program.cs
@@ -0,0 +1,39 @@
+﻿//  Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+//  m = 3, n = 4.
+//  0,5 7 -2 -0,2
+//  1 -3,3 8 -9,9
+//  8 7,8 -7,1 9
+double[,] GetVoidRandMatrix(int min, int max)
+{
+    int rows = 0, cols = 0;
+    rows = new Random().Next(min, max + 1);
+    cols = new Random().Next(min, max + 1);
+    double[,] outputMatrix = new double[rows, cols];
+    return outputMatrix;
+}
+
+void PrintMatrix(double[,] inputMatrix)
+{
+    for (int i = 0; i < inputMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputMatrix.GetLength(1); j++)
+        {
+            Console.Write($"{inputMatrix[i, j]:f2}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] FillMatrix(double[,] inputMatrix)
+{
+    for (int i = 0; i < inputMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputMatrix.GetLength(1); j++)
+        {
+            inputMatrix[i, j] = new Random().NextDouble() * 100;
+        }
+    }
+    return inputMatrix;
+}
+
+PrintMatrix(FillMatrix(GetVoidRandMatrix(3, 4)));
