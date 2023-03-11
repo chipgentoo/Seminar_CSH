5ff6d320764cf9dc817b5727cd57449184da57ea
chipgentoo <chip.gentoo@gmail.com>
2023-03-12 01:01:35 +0300

    Recursive list of numbers into a string

--- HomeWork_Exercise_64/Program.cs
+++ HomeWork_Exercise_64/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 64
+// Задайте значение N.
+// Вывести все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+/// <summary>
+/// Ввод числа из терминала / консоли
+/// </summary>
+/// <param name="msg">Сообщение перед вводом</param>
+/// <returns>Целое число</returns>
+int InputNumber(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+/// <summary>
+/// Возвращает строку чисел от START до END
+/// </summary>
+/// <param name="start">Начальное значение</param>
+/// <param name="end">Конечное значение</param>
+/// <returns>Строка чисел вида: 1, 2, 3 ...</returns>
+string GetListNumbers(int start, int end)
+{
+    if (start < end) return "Начальное значение меньше конечного.";
+    if (start == end) return end.ToString();
+    return (start + ", " + GetListNumbers(start - 1, end));
+}
+Console.WriteLine(GetListNumbers(InputNumber("Введите начальное число: "), InputNumber("Введите конечное число: ")));
