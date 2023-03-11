4544d4d5c27e5a81d36971ae225acb9228c4bf53
chipgentoo <chip.gentoo@gmail.com>
2023-03-12 01:17:21 +0300

    Recursive sum of numbers between...

---
+++ HomeWork_Exercise_66/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 66:
+// Задать значения M и N.
+// Найти сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+/// <summary>
+/// Ввод числа из терминала / консоли
+/// </summary>
+/// <param name="msg">Сообщение перед вводом</param>
+/// <returns>Целое число</returns>
+int InputNumber(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+/// <summary>
+/// Подсчет суммы чисел в промежутке от
+/// </summary>
+/// <param name="start">Начальное значение</param>
+/// <param name="end">Конечное значение</param>
+/// <returns>Сумму чисел</returns>
+int GetSumNumbers(int start, int end)
+{
+    if (start == end) return end;
+    if (start < end)
+    {
+        return (start + GetSumNumbers(start + 1, end));
+    }
+    else
+    {
+        return (start + GetSumNumbers(start - 1, end));
+    }
+}
+
+// Проверка
+Console.WriteLine(GetSumNumbers(InputNumber("Введите начальное число: "), InputNumber("Введите конечное число: ")));
